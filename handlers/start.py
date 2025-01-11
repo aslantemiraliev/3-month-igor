3ded94c59e4ab6cdfe652c890ef22c53fa584630
aslantemiraliev <temitalievaslan7@gmail.com>
2025-01-11 13:29:16 +0300

    fixed dz2

--- handlers/start.py
+++ handlers/start.py
@@ -6,8 +6,8 @@ from aiogram import types
 start_router = Router()
 
 
-@start_router.message (Command ('start' ))
+@start_router.message(Command('start'))
 async def start_handler(message: types.Message) :
   name = message.from_user.first_name
-  await message. answer (f'Привет {name}\n'
-                         f'Мои комманды: ')
+  await message.answer(f'Привет {name}\n'
+                         f'Мои комманды:')
